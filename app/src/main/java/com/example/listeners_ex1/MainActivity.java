43f08a65d1088734f6b5eb547a9c909334881eda
ad6354 <149888032+Ofd123@users.noreply.github.com>
2024-09-14 13:37:52 +0300

    finished the main screen's XML

--- app/src/main/java/com/example/listeners_ex1/MainActivity.java
+++ app/src/main/java/com/example/listeners_ex1/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.listeners_ex1;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,6 +11,9 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity
 {
+    Button redTeam,BlueTeam;
+    int redCount,blueCount;
+
 
 
     @Override
@@ -23,5 +27,8 @@ public class MainActivity extends AppCompatActivity
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        redTeam,BlueTeam;
+
     }
 }
